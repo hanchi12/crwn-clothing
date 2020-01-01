7358d7598c43c02b2a0744dfc8a7570de3a17492
Michael Cane <mvcane17@gmail.com>
2020-01-01 17:17:27 +0800

    update our menu-items to navigate withRouter HOC

--- src/component/directory/directory-component.jsx
+++ src/component/directory/directory-component.jsx
@@ -9,11 +9,11 @@ constructor(){
 
     this.state={
         sections:[
-            {title:'hats',imageUrl:'https://i.ibb.co/cvpntL1/hats.png',id:1},
-            {title:'jackets',imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',id:2},
-            {title:'sneakers',imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',id:3},
-            {title:'womens',imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',id:4,size:'large'},
-            {title:'mens',imageUrl:'https://i.ibb.co/R70vBrQ/men.png',id:5,size:'large'}
+            {title:'hats',imageUrl:'https://i.ibb.co/cvpntL1/hats.png',id:1,linkUrl:'hats'},
+            {title:'jackets',imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',id:2,linkUrl:''},
+            {title:'sneakers',imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',id:3,linkUrl:''},
+            {title:'womens',imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',id:4,size:'large',linkUrl:''},
+            {title:'mens',imageUrl:'https://i.ibb.co/R70vBrQ/men.png',id:5,size:'large',linkUrl:''}
         ]
     };
 
@@ -25,8 +25,8 @@ render(){
         <div className='directory-menu'>
             {
                 this.state.sections.map( 
-                    ( {title,imageUrl,id,size} ) => (
-                        <MenuItem key={id} imageUrl={imageUrl} title={title} size={size} ></MenuItem>
+                    ( {id,...otherSectionProps} ) => (
+                        <MenuItem key={id} {...otherSectionProps} ></MenuItem>
                     ) 
                 )
             }
